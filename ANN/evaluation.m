3338784776465fdb3d73fe216e6da4b8ac3c9794
Jose <jose.kalladanthyil11@imperial.ac.uk>
2013-11-20 15:17:23 +0000

    finished evaluation

--- ANN/evaluation.m
+++ ANN/evaluation.m
@@ -22,26 +22,51 @@ function [information] = evaluation(examples, targets, net)
     for i = 1:total_folds
         nans = 0;
         statistics.conf_matrix = statistics.conf_matrix +classifiers{i}.conf_matrix;
-        statistics.avg_classification_rate = statistics.avg_classification_rate +classifiers{i}.
+        statistics.avg_classification_rate = statistics.avg_classification_rate +classifiers{i}.error_rate;
+        for j = 1:total_classes
+            if classifiers{i}.falpha_measures(j) == -1
+                nans = nans+1;
+            else
+                statistics.avg_class_falpha_measure(i) = statistics.avg_class_falpha_measure(i) + classifiers{i}.falpha_measures(j)
+            end
+        end
+        statistics.avg_class_falpha_measure(i) = statistics.avg_class_falpha_measure(i) / total_classes-nans; 
+
     end
     
     %calculate the confusion matrix and classification rate. + initialises
     %other variables
     statistics.conf_matrix = statistics.conf_matrix / total_folds;
-    statistics.avg_classification_rate = 1 - sum([class_data.err_rate])/total_folds;
+    statistics.avg_classification_rate = 1 - statistics.avg_classification_rate/total_folds;
     statistics.avg_recall_rates = zeros(1,total_classes);
     statistics.avg_precision_rate = zeros(1,total_classes);
-    statistics.avg_falpha_measure = zeros(1,total_classes);
+    statistics.avg_fold_falpha_measure = zeros(1,total_classes);
     
     
     % calculated the avg  recall rate, f1 measure and precision rate for
     % each classification
     for i = 1:total_classes
+        nans = zeros(1,3);
         for j = 1:total_folds
-    
+            if classifiers{j}.precision_rates(i) == -1
+                nans(1) = nans(1)+1
+            else
+                statistics.avg_precision_rate(i) = statistics.avg_precision_rate(i) + classifiers{j}.precision_rates(i);
+            end            
+            if classifiers{j}.recall_rates(i) == -1
+                nans(2) = nans(2)+1
+            else
+                statistics.avg_recall_rates(i) = statistics.avg_recall_rates(i) + classifiers{j}.recall_rates(i);
+            end   
+            if classifiers{j}.falpha_measures(i) == -1
+                nans(3) = nans(3)+1
+            else
+                statistics.avg_fold_falpha_measure(i) = statistics.avg_fold_falpha_measure(i) + classifiers{j}.falpha_measures(i);
+            end
+
         end
-        statistics.avg_recall_rates(i) = statistics.avg_recall_rates(i)/total_folds;
-        statistics.avg_precision_rate(i) = statistics.avg_precision_rate(i) /total_folds;
-        statistics.avg_falpha_measure(i) = statistics.avg_falpha_measure(i) /total_folds;
+        statistics.avg_recall_rates(i) = statistics.avg_recall_rates(i)/(total_folds-nans(2);
+        statistics.avg_precision_rate(i) = statistics.avg_precision_rate(i) /(total_folds-nans(1);
+        statistics.avg_fold_falpha_measure(i) = statistics.avg_fold_falpha_measure(i) /(total_folds-nans(3));
     end
 end
